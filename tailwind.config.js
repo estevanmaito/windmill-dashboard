5823557bd3398adf51ffa6959fe31d3fa5d16952
Estevan Maito <ejmaito@gmail.com>
2020-06-09 22:09:10 -0300

    add shadow-outline plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,37 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
+const Color = require('color')
+
 module.exports = {
   purge: ['public/*.html'],
   theme: {
     themeVariants: ['dark'],
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     backgroundColor: ['dark'],
     textColor: ['dark'],
   },
-  plugins: [require('tailwindcss-multi-theme')],
+  plugins: [
+    require('tailwindcss-multi-theme'),
+    require('@tailwindcss/custom-forms'),
+    plugin(({ addUtilities, e, theme, variants }) => {
+      const newUtilities = {}
+      Object.entries(theme('colors')).map(([name, value]) => {
+        if (name === 'transparent' || name === 'current') return
+        const color = value[300] ? value[300] : value
+        const hsla = Color(color).alpha(0.45).hsl().string()
+
+        newUtilities[`.shadow-outline-${name}`] = {
+          'box-shadow': `0 0 0 3px ${hsla}`,
+        }
+      })
+
+      addUtilities(newUtilities, variants('boxShadow'))
+    }),
+  ],
 }

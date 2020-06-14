6c3d96ac1c6a0ffe0bab4a9bb9471ac15f6d7231
Estevan Maito <ejmaito@gmail.com>
2020-06-14 13:03:08 -0300

    add charts to external files

---
+++ public/assets/js/charts-pie.js
@@ -0,0 +1,35 @@
+/**
+ * For usage, visit Chart.js docs https://www.chartjs.org/docs/latest/
+ */
+const pieConfig = {
+  type: 'doughnut',
+  data: {
+    datasets: [
+      {
+        data: [33, 33, 33],
+        /**
+         * These colors come from Tailwind CSS palette
+         * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
+         */
+        backgroundColor: ['#38B2AC', '#4299E1', '#805AD5'],
+        label: 'Dataset 1',
+      },
+    ],
+    labels: ['Shoes', 'Shirts', 'Bags'],
+  },
+  options: {
+    responsive: true,
+    cutoutPercentage: 80,
+    /**
+     * Default legends are ugly and impossible to style.
+     * See examples in charts.html to add your own legends
+     *  */
+    legend: {
+      display: false,
+    },
+  },
+}
+
+// change this to the id of your chart element in HMTL
+const pieCtx = document.getElementById('pie')
+window.myPie = new Chart(pieCtx, pieConfig)

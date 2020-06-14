f698a5aa0a4f3786330f73a49300bdb6360a18d7
Estevan Maito <ejmaito@gmail.com>
2020-06-14 13:48:58 -0300

    add charts page

---
+++ public/assets/js/charts-bars.js
@@ -0,0 +1,34 @@
+/**
+ * For usage, visit Chart.js docs https://www.chartjs.org/docs/latest/
+ */
+const barConfig = {
+  type: 'bar',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Shoes',
+        backgroundColor: '#0694a2',
+        // borderColor: window.chartColors.red,
+        borderWidth: 1,
+        data: [-3, 14, 52, 74, 33, 90, 70],
+      },
+      {
+        label: 'Bags',
+        backgroundColor: '#7e3af2',
+        // borderColor: window.chartColors.blue,
+        borderWidth: 1,
+        data: [66, 33, 43, 12, 54, 62, 84],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    legend: {
+      display: false,
+    },
+  },
+}
+
+const barsCtx = document.getElementById('bars')
+window.myBar = new Chart(barsCtx, barConfig)

0945b590fbc5823f7d4cb9e09e805ace5aab161a
Cesar Estrada <cesar@codigomovil.mx>
2021-03-16 20:46:24 -0500

    Fixed variants for v2.0.3 extending rather than replacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,26 @@ module.exports = {
 		},
 	},
 	variants: {
-		extend: {},
+		extend: {
+			backgroundColor: [
+				'hover',
+				'focus',
+				'active',
+				'odd',
+				'dark',
+			],
+			display: [ 'responsive', 'dark' ],
+			textColor: [
+				'focus-within',
+				'hover',
+				'active',
+				'dark',
+			],
+			placeholderColor: [ 'focus', 'dark' ],
+			borderColor: [ 'focus', 'hover', 'dark' ],
+			divideColor: [ 'dark' ],
+			boxShadow: [ 'focus' ],
+		},
 	},
 	plugins: [
 		require('@tailwindcss/forms'),

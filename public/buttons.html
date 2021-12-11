1854ed760fa7b3e2473569b5e75f04e556438378
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2021-12-11 13:31:54 +0700

    fix: update to tailwind 3

--- public/buttons.html
+++ public/buttons.html
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+<html :class="{ 'dark': dark }" x-data="data()" lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ const plugin = require('tailwindcss/plugin')
 const Color = require('color')
 
 module.exports = {
-  purge: ['public/**/*.html'],
+  content: ['public/**/*.html'],
+  darkMode: 'class',
   theme: {
-    themeVariants: ['dark'],
     customForms: (theme) => ({
       default: {
         'input, textarea': {
@@ -170,36 +170,13 @@ module.exports = {
       },
     },
   },
-  variants: {
-    backgroundColor: [
-      'hover',
-      'focus',
-      'active',
-      'odd',
-      'dark',
-      'dark:hover',
-      'dark:focus',
-      'dark:active',
-      'dark:odd',
-    ],
-    display: ['responsive', 'dark'],
-    textColor: [
-      'focus-within',
-      'hover',
-      'active',
-      'dark',
-      'dark:focus-within',
-      'dark:hover',
-      'dark:active',
-    ],
-    placeholderColor: ['focus', 'dark', 'dark:focus'],
-    borderColor: ['focus', 'hover', 'dark', 'dark:focus', 'dark:hover'],
-    divideColor: ['dark'],
-    boxShadow: ['focus', 'dark:focus'],
-  },
   plugins: [
-    require('tailwindcss-multi-theme'),
-    require('@tailwindcss/custom-forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    }),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
     plugin(({ addUtilities, e, theme, variants }) => {
       const newUtilities = {}
       Object.entries(theme('colors')).map(([name, value]) => {

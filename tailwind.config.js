8bd03fbe0000453fd8aa7772888ce7442087a94a
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-01-22 17:39:02 +0700

    fix: add back shadow-xs and shadow-outline for tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -168,6 +168,10 @@ module.exports = {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
+      boxShadow: {
+        xs: '0 0 0 1px rgba(0, 0, 0, 0.05)',
+        outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+      }
     },
   },
   plugins: [

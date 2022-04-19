9a7cfe41086463d310fb582717d0401a45bf11f6
Cesar Estrada <cesar@codigomovil.mx>
2022-04-19 14:07:21 -0500

    Update TW3.0.24

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,60 +2,44 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-	purge: [
-		'./public/**/*.html',
-		'./public/**/*.{js,jsx}'
-	],
-	darkMode: 'class', // or 'media' or 'class'
-	theme: {
-		customForms: (theme) => ({
-			default: {
-				'input, textarea': {
-					'&::placeholder': {
-						color: theme('colors.gray.400'),
-					},
-				},
-			},
-		}),
-		extend: {
-			colors: {
-				primary: colors.purple,
-				teal: colors.teal,
-				orange: colors.orange,
-				coolGray: colors.coolGray
-			},
-			maxHeight: {
-				xl: '36rem',
-			},
-			fontFamily: {
-				sans: ['Inter', ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	variants: {
-		extend: {
-			backgroundColor: [
-				'hover',
-				'focus',
-				'active',
-				'odd',
-				'dark',
-			],
-			display: ['responsive', 'dark'],
-			textColor: [
-				'focus-within',
-				'hover',
-				'active',
-				'dark',
-			],
-			placeholderColor: ['focus', 'dark'],
-			borderColor: ['focus', 'hover', 'dark'],
-			divideColor: ['dark'],
-			boxShadow: ['focus'],
-		},
-	},
-	plugins: [
-		require('@tailwindcss/jit'),
-		require('@tailwindcss/forms'),
-	],
+  content: [
+    './public/**/*.html',
+    './public/**/*.{js,jsx}'
+  ],
+  darkMode: 'class',
+  theme: {
+    customForms: (theme) => ({
+      default: {
+        'input, textarea': {
+          '&::placeholder': {
+            color: theme('colors.gray.400'),
+          },
+        },
+      },
+    }),
+    extend: {
+      colors: {
+        primary: colors.purple,
+        teal: colors.teal,
+        orange: colors.orange,
+        coolGray: colors.coolGray
+      },
+      maxHeight: {
+        xl: '36rem',
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        xs: '0 0 0 1px rgba(0, 0, 0, 0.05)',
+        outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    }),
+  ],
 }

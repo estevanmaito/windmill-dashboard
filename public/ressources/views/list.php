dcda960eff7ace156996d72e715134f2201cb89d
Yousra BARHMI <barhmiyousra@gmail.com>
2023-07-11 15:46:16 +0200

    icon buttons fixed

--- public/ressources/views/list.php
+++ public/ressources/views/list.php
@@ -74,15 +74,42 @@ ob_start();
           <td class="px-4 py-3 text-sm"><?= $user->getUserEmail() ?></td>
           <td class="px-4 py-3 text-sm"><?= $user->getRegistrationTime() ?></td>
           <td class="px-4 py-3 text-sm">
-       
-        
-          <div> <button class="flex items-center justify-between px-2 py-2 text-sm font -medium leading-5 text-white transition-colors duration-150 bg-purple-6 00 border border-transparent rounded-full active:bg-purple-600 hover:bg -purple-700 focus:outline-none focus: shadow-outline-purple" aria-label= "Edit"> flex
-              <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox= "0 0 20 20">
-                <path d="M13.586 3.586a2 2 0 112.828 2.8281-.793.793-2.828-2.828.79 3-.793ZM11.379 5.793L3 14.172V17h2.82818.38-8.379-2.83-2.828z">
-                </path>
-              </svg>
-            </button>
+            <div class="flex items-center space-x-4 text-sm">
+              <a
+                class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                aria-label="Edit" href="index1.php?action=edit&id=<?php echo $user->getId()?>"
+              >
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                  ></path>
+                </svg>
+      </a>
+              <a
+                class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                aria-label="Delete" onclick="return confirm('voulez vous vraiment supprimer ce utilisateur')"  href="index1.php?action=destroy&id=<?php echo $user->getId()?>"
+              >
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+      </a>
           </div>
+
+
           </td>
 
         </tr>

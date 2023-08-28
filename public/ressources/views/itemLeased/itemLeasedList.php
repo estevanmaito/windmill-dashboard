bcfe904d6f1bf503d7248795b78b000bc160e2b6
Yousra BARHMI <barhmiyousra@gmail.com>
2023-08-28 14:29:22 +0200

    mise à jour des donnees

--- public/ressources/views/itemLeased/itemLeasedList.php
+++ public/ressources/views/itemLeased/itemLeasedList.php
@@ -50,7 +50,7 @@ $endItem = min($startItem + $itemsPerPage - 1, $totalItems);
           <div>
             <p
               class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"  >
-              Total clients
+              Total reservations
             </p>
             
             <p class="text-lg font-semibold text-gray-700 dark:text-gray-200" >
--- public/sam.sql
+++ public/sam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 25 août 2023 à 13:26
+-- Généré le : lun. 28 août 2023 à 12:28
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
